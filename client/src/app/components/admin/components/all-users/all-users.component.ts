330bfe1b75d16403cb64d1129caa2e17915ba8b1
Trejgo Sergej <konnon69@gmail.com>
2022-05-05 18:02:04 +0300

    style: list users

--- client/src/app/components/admin/components/all-users/all-users.component.ts
+++ client/src/app/components/admin/components/all-users/all-users.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy,Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { UserService } from 'src/app/services/user.service';
 import { User } from 'src/app/interfaces/User';
@@ -6,7 +6,8 @@ import { User } from 'src/app/interfaces/User';
 @Component({
   selector: 'app-all-users',
   templateUrl: './all-users.component.html',
-  styleUrls: ['./all-users.component.scss']
+  styleUrls: ['./all-users.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AllUsersComponent implements OnInit {
 
