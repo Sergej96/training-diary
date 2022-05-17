1cc9bcf667749fc73afb278146424a69715b147a
Trejgo Sergej <konnon69@gmail.com>
2022-05-17 11:58:52 +0300

    add: exercise-details

---
+++ client/src/app/components/pages/exercise-guide/exercise-details/exercise-details.component.ts
@@ -0,0 +1,40 @@
+import { Component, Input, OnDestroy, OnInit } from "@angular/core";
+import { ActivatedRoute, Params } from "@angular/router";
+import { Observable, of, Subject, switchMap, takeUntil } from "rxjs";
+import { ExerciseInfo } from "src/app/interfaces/ExerciseInfo";
+import { Muscle } from "src/app/interfaces/Muscle";
+import { ExercisesService } from "src/app/services/exercises.service";
+
+@Component({
+  selector: 'app-exercise-details',
+  templateUrl: './exercise-details.component.html',
+  styleUrls: ['./exercise-details.component.scss']
+})
+export class ExerciseDetailsComponent implements OnInit {
+
+  private destroy_: Subject<unknown> = new Subject()
+
+  exerciseInfo$!: Observable<ExerciseInfo | null>
+
+  constructor(
+    private route: ActivatedRoute,
+    private exerciseService: ExercisesService
+  ) { }
+  ngOnInit(): void {
+    this.exerciseInfo$ = this.route.params
+      .pipe(
+        switchMap((params: Params) => {
+          if (params['id']) {
+            return this.exerciseService.getById(params['id'])
+          }
+          return of(null)
+        })
+      )
+  }
+
+  showMuscle(item: Muscle){
+    return " " + item.name
+  }
+
+
+}
